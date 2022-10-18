f2fc74ab377afa875f29944c14c61230be54122e
tampt <tampt95@gmail.com>
2022-10-18 22:50:16 +0700

    add morgan package

--- apps/authenticated/src/main.ts
+++ apps/authenticated/src/main.ts
@@ -7,20 +7,24 @@ import * as express from "express";
 import routers from "./app/routers";
 import * as cors from "cors";
 import * as bodyParser from "body-parser";
+import * as morgan from "morgan";
 import mongoose from "mongoose";
 import { environment } from "apps/authenticated/src/environments/environment";
 const app = express();
 
 app.use(cors());
+app.use(morgan("tiny"));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
-app.use(routers);
+app.use("/api", routers);
 
 const port = process.env.port || 3333;
 const server = app.listen(port, async () => {
-  console.log(`mongodb+srv://${environment.dbUsername}:${environment.dbPassword}@${environment.dbCluster}.mongodb.net/${environment.dbName}?retryWrites=true&w=majority`)
+  console.log(
+    `mongodb+srv://${environment.dbUsername}:${environment.dbPassword}@${environment.dbCluster}.mongodb.net/${environment.dbName}?retryWrites=true&w=majority`
+  );
   await mongoose.connect(
     `mongodb+srv://${environment.dbUsername}:${environment.dbPassword}@${environment.dbCluster}.mongodb.net/${environment.dbName}?retryWrites=true&w=majority`
   );
