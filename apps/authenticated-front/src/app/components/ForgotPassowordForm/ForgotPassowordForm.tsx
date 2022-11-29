3ae35980c846db82456c5c892a5177169ad9211b
tampt <phamthanhtam020295@gmail.com>
2022-11-29 07:22:43 +0700

    update forgot password form and validate email

--- apps/authenticated-front/src/app/components/ForgotPassowordForm/ForgotPassowordForm.tsx
+++ apps/authenticated-front/src/app/components/ForgotPassowordForm/ForgotPassowordForm.tsx
@@ -1,7 +1,7 @@
 import Spinner from "apps/authenticated-front/src/app/components/Spinner/Spinner";
 import { useForm } from "react-hook-form";
 interface ForgotPasswordProps {
-  onSubmit: (email: string, password: string) => void;
+  onSubmit: (email: string) => void;
 }
 
 type ForgotPassowordFormValue = { email: string };
@@ -10,27 +10,38 @@ export default ({ onSubmit }: ForgotPasswordProps) => {
   const {
     register,
     handleSubmit,
-    formState: { isSubmitting },
+    formState: { isSubmitting, errors },
   } = useForm<ForgotPassowordFormValue>();
-  const submitHandler = ({ email }: ForgotPassowordFormValue) => {
-    console.log(email);
-  };
+  
+
   return (
-    <form onSubmit={handleSubmit(submitHandler)}>
-      <div className="space-y-4">
-        <div>
-          <label className="block mb-1">Email</label>
-          <input
-            className="block w-full rounded-md"
-            {...register("email", { required: true })}
-            type="email"
-            placeholder="you@awesome.com"
-          />
+    <form onSubmit={handleSubmit(({email}: ForgotPassowordFormValue)=>{onSubmit(email)})}>
+      <div className="pb-4">
+        <label className="block mb-">Email</label>
+        <div className="relative">
+        <input
+          className="block w-full rounded-md"
+          {...register("email", {
+            required: "Please enter a valid email",
+            pattern: {
+              value:
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              message: "Please enter a valid email",
+            },
+          })}
+          type="text"
+          placeholder="you@awesome.com"
+        />
+        {errors.email?.message && (
+          <div className="text-sm text-red-500 absolute left-0 top-full">
+            {errors.email.message}
+          </div>
+        )}
         </div>
       </div>
       <button
         disabled={isSubmitting}
-        className="relative mt-8 text-center bg-slate-600 w-full block py-2 text-white font-semibold rounded-md"
+        className="relative mt-4 text-center bg-slate-600 w-full block py-2 text-white font-semibold rounded-md"
       >
         {isSubmitting ? (
           <div className="absolute top-1/2 right-2 -translate-y-1/2">
